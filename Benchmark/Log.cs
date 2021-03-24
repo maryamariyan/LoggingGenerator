0c2d3ed44fb238d9ff840cb62a7205792ddf6143
Maryam Ariyan <maryam.ariyan@microsoft.com>
2021-03-23 20:44:55 -0400

    benchmarks (#7)
    * benchmarks
    
    * Update Benchmark/Log.cs
    
    * Update Benchmark/Program.cs

---
+++ Benchmark/Log.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    static partial class Log
+    {
+        [LoggerMessage(380, LogLevel.Debug, @"Connection id '{connectionId}' received {type} frame for stream ID {streamId} with length {length} and flags {flags} and {other}")]
+        public static partial void LogTest(ILogger logger, string connectionId, string type, string streamId, string length, string flags, string flagsd);
+    }
+}
